b8c93641388b6df7a683f7cd97a4b0a8a4cd8f29
Nurbala Kaldybayeva <nkaldybayeva@gmail.com>
2018-08-07 17:36:00 -0400

    Changes in pomxml file and in testng runner.xml files

--- src/test/java/com/myfitnesspal/pages/ExercisePage.java
+++ src/test/java/com/myfitnesspal/pages/ExercisePage.java
@@ -1,66 +1,52 @@
 package com.myfitnesspal.pages;
 
-<<<<<<< HEAD
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.myfitnesspal.utilities.Driver;
-=======
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 
 import com.myfitnesspal.utilities.*;
->>>>>>> 9785c408a1d903fc93119b99aa60acd6523a11fd
 
 public class ExercisePage {
 
-public ExercisePage() {
+	public ExercisePage() {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
-<<<<<<< HEAD
-	
-	@FindBy(linkText="EXERCISE")
-=======
 
-@FindBy(xpath="//i[@id='datepicker-trigger']")
-public WebElement calendar;
+	@FindBy(xpath = "//i[@id='datepicker-trigger']")
+	public WebElement calendar;
 
-@FindBy(xpath="//div[@class='diary']//span/time")
-public WebElement calendarDate;
+	@FindBy(xpath = "//div[@class='diary']//span/time")
+	public WebElement calendarDate;
 
-//   
+	//
 
-
-	@FindBy(id="ember1431")
->>>>>>> 9785c408a1d903fc93119b99aa60acd6523a11fd
+	@FindBy(linkText = "EXERCISE")
 	public WebElement exerciseTab;
-	
-	@FindBy(linkText="Add Exercise")
+
+	@FindBy(linkText = "Add Exercise")
 	public WebElement addExercise;
-	
-	@FindBy(id="search")
+
+	@FindBy(id = "search")
 	public WebElement searchExercise;
-	
-	@FindBy(xpath="//input[@type='submit']")
+
+	@FindBy(xpath = "//input[@type='submit']")
 	public WebElement searchExerciseButton;
-	
-	@FindBy(xpath="//p[@class='flash']")
+
+	@FindBy(xpath = "//p[@class='flash']")
 	public WebElement flashErrorText;
-	
-	@FindBy(xpath="(//ul[@id='matching']//a[contains(.,'Run')])[3]")
+
+	@FindBy(xpath = "(//ul[@id='matching']//a[contains(.,'Run')])[3]")
 	public WebElement matchingResult;
-	
-	@FindBy(id="exercise_entry_quantity")
+
+	@FindBy(id = "exercise_entry_quantity")
 	public WebElement entryQuantity;
-	
-	@FindBy(id="update_exercise")
+
+	@FindBy(id = "update_exercise")
 	public WebElement updateExercise;
-	
-	@FindBy(id="exercise_entry_calories")
+
+	@FindBy(id = "exercise_entry_calories")
 	public WebElement entryCalory;
-	
-	@FindBy(xpath="//table[@class='table0']//tbody/tr/td[3]")
+
+	@FindBy(xpath = "//table[@class='table0']//tbody/tr/td[3]")
 	public WebElement tableCalory;
 
 }
